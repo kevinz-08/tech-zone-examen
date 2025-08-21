6a9978132ff23acd56b72f0eec3b576fb5b89edc
“kevinz-08” <gutierrezamorochokevinsantiago@gmail.com>
2025-08-21 07:18:46 -0500

    first commit

---
+++ db.sql
@@ -0,0 +1,52 @@
+CREATE TABLE "clientes"(
+    "id_cliente" SERIAL NOT NULL,
+    "nombre" VARCHAR(255) NOT NULL,
+    "correo" BIGINT NOT NULL,
+    "telefono" BIGINT NOT NULL
+);
+ALTER TABLE
+    "clientes" ADD PRIMARY KEY("id_cliente");
+CREATE TABLE "productos"(
+    "id_producto" SERIAL NOT NULL,
+    "nombre" VARCHAR(255) NOT NULL,
+    "categoria" VARCHAR(255) NOT NULL,
+    "precio" VARCHAR(255) CHECK
+        ("precio" IN('')) NOT NULL,
+        "stock" BOOLEAN NOT NULL,
+        "id_proveedor" SERIAL NOT NULL
+);
+ALTER TABLE
+    "productos" ADD PRIMARY KEY("id_producto");
+CREATE TABLE "ventas"(
+    "id_venta" SERIAL NOT NULL,
+    "fecha" DATE NOT NULL,
+    "id_cliente" SERIAL NOT NULL
+);
+ALTER TABLE
+    "ventas" ADD PRIMARY KEY("id_venta");
+CREATE TABLE "proveedores"(
+    "id_proveedor" SERIAL NOT NULL,
+    "nombre" VARCHAR(255) NOT NULL,
+    "correo" VARCHAR(255) NOT NULL,
+    "telefono" VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    "proveedores" ADD PRIMARY KEY("id_proveedor");
+CREATE TABLE "detalle_venta"(
+    "id_detalle" SERIAL NOT NULL,
+    "id_venta" SERIAL NOT NULL,
+    "id_producto" SERIAL NOT NULL,
+    "cantidad" INTEGER NOT NULL,
+    "precio" VARCHAR(255) CHECK
+        ("precio" IN('')) NOT NULL
+);
+ALTER TABLE
+    "detalle_venta" ADD PRIMARY KEY("id_detalle");
+ALTER TABLE
+    "ventas" ADD CONSTRAINT "ventas_id_cliente_foreign" FOREIGN KEY("id_cliente") REFERENCES "clientes"("id_cliente");
+ALTER TABLE
+    "detalle_venta" ADD CONSTRAINT "detalle_venta_id_venta_foreign" FOREIGN KEY("id_venta") REFERENCES "ventas"("id_venta");
+ALTER TABLE
+    "detalle_venta" ADD CONSTRAINT "detalle_venta_id_producto_foreign" FOREIGN KEY("id_producto") REFERENCES "productos"("id_producto");
+ALTER TABLE
+    "productos" ADD CONSTRAINT "productos_id_proveedor_foreign" FOREIGN KEY("id_proveedor") REFERENCES "proveedores"("id_proveedor");
